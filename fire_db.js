b0efaddee5cddf3dd341d58d32f1168554fea407
prabalp <prabalpanda2008@gmail.com>
2021-08-12 17:36:10 +0530

    first commit

---
+++ fire_db.js
@@ -0,0 +1,17 @@
+const db = firebase.database();
+
+export const add_db = async () => {
+  // add new entry to the db
+};
+
+export const read_db = async () => {
+  //read from db
+};
+
+export const update_db = async () => {
+  //update from db
+};
+
+export const delete_db = async () => {
+  //delete from db
+};
